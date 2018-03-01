654c24aad7b8c77dae1db8aacaf06b1efd9c4045
Thiago Loureiro <escazi1@gmail.com>
2018-02-28 23:29:02 -0500

    jwt plugin + initial react + initial theme + CORS

---
+++ html/front/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  componentWillMount() {
+    fetch('https://api.headless.dev')
+      .then(response => {
+        if (response.ok) {
+          return Promise.resolve(response);
+        }
+        else {
+          return Promise.reject(new Error('Failed to load'));
+        }
+      })
+      .then(response => response.json()) // parse response as JSON
+      .then(data => {
+        console.log(data);
+      })
+      .catch(function(error) {
+        console.log(`Error: ${error.message}`);
+      });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
