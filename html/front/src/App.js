d517ef0cb7f34cbdab2d2b2ba326f262e9f522c4
Thiago Loureiro <thiago@tloureiro.com>
2018-03-14 22:20:32 -0400

    everything except getSettings

--- html/front/src/App.js
+++ html/front/src/App.js
@@ -6,42 +6,13 @@ class App extends Component {
   static apiBaseURL = 'https://api.headless.localhost';
 
   state = {
-    isAuthorized: '',
+    verifyLoginMessage: '',
+    loginMessage: '',
+    logoutMessage: '',
+    checkSettingsMessage: '',
   };
-  componentWillMount() {
-    const data = {
-      username: 'admin',
-      password: 'admin',
-    };
 
-    fetch(`${App.apiBaseURL}/wp-json/jwt-auth/v1/token`, {
-      body: JSON.stringify(data), // must match 'Content-Type' header
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      headers: {
-        'content-type': 'application/json',
-      },
-      method: 'POST', // *GET, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *same-origin
-      redirect: 'follow', // *manual, error
-      referrer: 'no-referrer', // *client
-    }).then(response => response.json())
-      .then((json) => {
-        Cookies.set('token', json.token);
-        // fetch('https://api.headless.localhost/wp-json/wp/v2/settings', {
-        //   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        //   headers: {
-        //     Authorization: `Bearer ${json.token}`,
-        //     'content-type': 'application/json',
-        //   },
-        //   method: 'GET', // *GET, PUT, DELETE, etc.
-        //   mode: 'cors', // no-cors, *same-origin
-        //   redirect: 'follow', // *manual, error
-        //   referrer: 'no-referrer', // *client
-        // }).then(response => response.json());
-      });
-  }
-
-  verifyAuth = () => {
+  verifyLogin = () => {
     const token = Cookies.get('token');
 
     if (token) {
@@ -59,14 +30,85 @@ class App extends Component {
         .then(response => response.json())
         .then((json) => {
           if (json.code === 'jwt_auth_valid_token') {
-            this.setState({ isAuthorized: 'yes' });
+            this.setState({ verifyLoginMessage: 'Logged in' });
           } else {
-            this.setState({ isAuthorized: 'no' });
+            this.setState({ verifyLoginMessage: 'Not logged in' });
           }
         });
+    } else {
+      this.setState({ verifyLoginMessage: 'Not logged in' });
     }
+  };
 
+  login = () => {
+    const data = {
+      username: 'admin',
+      password: 'admin',
+    };
 
+    fetch(`${App.apiBaseURL}/wp-json/jwt-auth/v1/token`, {
+      body: JSON.stringify(data), // must match 'Content-Type' header
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      headers: {
+        'content-type': 'application/json',
+      },
+      method: 'POST', // *GET, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *same-origin
+      redirect: 'follow', // *manual, error
+      referrer: 'no-referrer', // *client
+    }).then(response => response.json())
+      .then((json) => {
+        if (json.token) {
+          console.log(json);
+          Cookies.set('token', json.token);
+          this.setState({
+            verifyLoginMessage: 'Logged in',
+            loginMessage: 'Success',
+          });
+        } else {
+          this.setState({
+            loginMessage: 'Failed',
+          });
+        }
+      });
+  };
+
+  logout = () => {
+    const token = Cookies.get('token');
+    Cookies.remove('token');
+
+    if (token) {
+      this.setState({
+        logoutMessage: 'Logged out',
+
+      });
+    } else {
+      this.setState({ logoutMessage: 'You are already logged out' });
+    }
+  };
+
+  checkSettings = () => {
+    const token = Cookies.get('token');
+
+    fetch('https://api.headless.localhost/wp-json/wp/v2/settings', {
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'content-type': 'application/json',
+      },
+      method: 'GET', // *GET, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *same-origin
+      redirect: 'follow', // *manual, error
+      referrer: 'no-referrer', // *client
+    }).then((response) => {
+      if (!response.ok) {
+        throw response;
+      }
+      return response.json();
+    }).then((json) => {
+      console.log(json);
+    }).catch(error => error.json())
+      .then((jsonError) => { console.log(jsonError); });
   };
 
   render() {
@@ -77,13 +119,23 @@ class App extends Component {
         </header>
         <div className="App-intro">
           <div>
-            <button onClick={this.verifyAuth}>Verify Auth</button>
-            <span>&nbsp;{this.state.isAuthorized}</span>
+            <button onClick={this.verifyLogin}>Verify Login</button>
+            <span>&nbsp;{this.state.verifyLoginMessage}</span>
           </div>
 
           <div>
-            <button onClick={this.verifyAuth}>Verify Auth</button>
-            <span>&nbsp;{this.state.isAuthorized}</span>
+            <button onClick={this.login}>Login</button>
+            <span>&nbsp;{this.state.loginMessage}</span>
+          </div>
+
+          <div>
+            <button onClick={this.logout}>Logout</button>
+            <span>&nbsp;{this.state.logoutMessage}</span>
+          </div>
+
+          <div>
+            <button onClick={this.checkSettings}>Check Settings</button>
+            <span>&nbsp;{this.state.checkSettingsMessage}</span>
           </div>
         </div>
       </div>
