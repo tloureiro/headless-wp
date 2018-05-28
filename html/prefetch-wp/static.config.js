ae4731d4a60cd73b941578a06bda7e52ec6be60a
Thiago Loureiro <escazi1@gmail.com>
2018-05-28 19:55:53 -0400

    new static config

--- html/prefetch-wp/static.config.js
+++ html/prefetch-wp/static.config.js
@@ -7,8 +7,25 @@ export default {
     title: 'React Static',
   }),
 
+  siteRoot: 'https://headless.localhost',
+
   async getRoutes() {
-    const posts = await this.getAllPosts();
+
+    let posts = [];
+
+    const firstPage = await axios.get('https://api.headless.localhost/wp-json/wp/v2/posts/?per_page=100&page=1');
+
+    const totalPages = parseInt(firstPage.headers['x-wp-totalpages']);
+
+    posts = posts.concat(firstPage.data);
+
+    if ( totalPages > 1 ) {
+      for (let i=2; i<=totalPages; i++){
+        const page = await axios.get(`https://api.headless.localhost/wp-json/wp/v2/posts/?per_page=100&page=${i}`);
+        posts = posts.concat(page.data);
+      }
+    }
+
     return [
       {
         path: '/',
@@ -29,24 +46,6 @@ export default {
         })),
       },
     ]
-  },
+  }
 
-  getAllPosts: async () => {
-    let posts = [];
-
-    const firstPage = await axios.get('https://api.headless.localhost/wp-json/wp/v2/posts/?per_page=100&page=1');
-
-    const totalPages = parseInt(firstPage.headers['x-wp-totalpages']);
-
-    posts = posts.concat(firstPage.data);
-
-    if ( totalPages > 1 ) {
-      for (let i=2; i<=totalPages; i++){
-        const page = await axios.get(`https://api.headless.localhost/wp-json/wp/v2/posts/?per_page=100&page=${i}`);
-        posts = posts.concat(page.data);
-      }
-    }
-
-    return posts;
-  },
 }
