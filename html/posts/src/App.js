d8d372ec58cb5f9f5d9f4bddf3cc574b9f41bd6f
Thiago Loureiro <escazi1@gmail.com>
2018-05-29 18:01:43 -0400

    posts showing

--- html/posts/src/App.js
+++ html/posts/src/App.js
@@ -1,30 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Router, Route, Switch, Link } from 'react-static'
 import { hot } from 'react-hot-loader'
-//
+
 import Home from 'containers/Home'
-import About from 'containers/About'
 import Blog from 'containers/Blog'
 import NotFound from 'containers/404'
 
 import './app.css'
 
-const App = () => (
-  <Router>
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/blog/">Blog</Link>
-      </nav>
-      <div className="content">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/blog" component={Blog} />
-          <Route component={NotFound} />
-        </Switch>
-      </div>
-    </div>
-  </Router>
-)
+class App extends Component {
+
+  render() {
+
+    return (
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/blog/">Blog</Link>
+          </nav>
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/blog" component={Blog} />
+              <Route component={NotFound}/>
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default hot(module)(App)
--- html/posts/src/containers/Blog.js
+++ html/posts/src/containers/Blog.js
@@ -1,14 +1,42 @@
-
 import React, { Component } from 'react'
-import { Switch, Route } from 'react-static'
+import { Switch, Route, Link } from 'react-static'
+import axios from 'axios'
 
 import Post from './Post'
 
 class Blog extends Component {
 
-  static apiBaseURL = 'https://api.headless.localhost';
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    this.retrievePosts();
+  }
+
+  retrievePosts = async () => {
+    let posts = [];
+
+    const firstPage = await axios.get('https://api.headless.localhost/wp-json/wp/v2/posts/?per_page=100&page=1');
+
+    const totalPages = parseInt(firstPage.headers['x-wp-totalpages']);
+
+    posts = posts.concat(firstPage.data);
+
+    if ( totalPages > 1 ) {
+      for (let i=2; i<=totalPages; i++){
+        const page = await axios.get(`https://api.headless.localhost/wp-json/wp/v2/posts/?per_page=100&page=${i}`);
+        posts = posts.concat(page.data);
+      }
+    }
+
+    this.setState( { posts } );
+  };
 
   render() {
+
+    const posts = this.state.posts;
+
     return (
       <div>
         <Switch>
